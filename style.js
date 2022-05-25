b681de0bdfbb1ffc85c30f77bd992055851710d1
Ender <sanchender1282@gmail.com>
2022-05-24 21:36:44 -0400

    dia 1

---
+++ style.js
@@ -0,0 +1,25 @@
+// Escribe una función que tome dos números (a y b) como argumento, Suma a y b. Devuelve el resultado
+
+// const myFuntion = (a, b) => {
+//   return a + b;
+// };
+// console.log(myFuntion(2, 2));
+
+// Escribe una función que tome dos valores, digamos a y b, como argumentos, Devuelve verdadero si los dos valores son iguales y del mismo tipo
+
+// const myFuntion = (a, b) => {
+//   if (a === b) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// };
+// console.log(myFuntion(2, "2"));
+
+// Escribir una función que tome un valor como argumento
+// Devuelve el tipo del valor
+
+// const myFuntion = (a) => {
+// return typeof (a)
+// }
+// console.log(myFuntion(""))
