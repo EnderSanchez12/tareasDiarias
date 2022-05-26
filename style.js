13f4ad49a8109a042d86ed8d489c1fa57fd1c2c1
Ender <sanchender1282@gmail.com>
2022-05-25 21:14:22 -0400

    Dia 2

--- style.js
+++ style.js
@@ -23,3 +23,32 @@
 // return typeof (a)
 // }
 // console.log(myFuntion(""))
+
+// Escribir una función que tome una cadena (a) y un número (n) como argumento
+// Devuelve el carácter enésimo de 'a'
+
+// function myFuntion(a, n) {
+//   return a[n - 1];
+// }
+
+// console.log(myFuntion("abcd", 1));
+
+
+
+// Escribir una función que tome una cadena (a) como argumento
+// Eliminar los primeros 3 caracteres de un
+// Devuelve el resultado
+
+// function myFuntion(a){
+//     return a.slice(3)
+// }
+// console.log(myFuntion('abcdefg'));
+
+// Escribir una función que tome una cadena como argumento
+// Extrae los últimos 3 caracteres de la cadena
+// Devuelve el resultado
+
+// function myFuntion(str){
+//     return str.slice(-3)
+// }
+// console.log(myFuntion('abcdefg'));
