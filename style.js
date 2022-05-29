44c0456891aeaf0ca8072b8ac5f11a67df8fc0ad
Ender <sanchender1282@gmail.com>
2022-05-29 09:41:00 -0400

    Dia 5 y 6

--- style.js
+++ style.js
@@ -101,6 +101,65 @@
 // Si el número es par, devuelve verdadero
 // De lo contrario, devuelve falso
 
-// const myFunction = (a) => a % 2 == 0 
+// const myFunction = (a) => a % 2 == 0
 
 // console.log(myFunction(14));
+
+// Escribe una función que tome dos cadenas (a y b) como argumentos
+// Devuelve el numero de veces que a ocurre en b
+
+// function myFunction(a, b) {
+//   return b.split("").filter((x) => x == a).length;
+// }
+// console.log(myFunction("m", "how mmm many times does the character occur in this sentence?"));
+
+// Escribir una función que tome un número (a) como argumento
+// Si a es un número entero (no tiene lugar decimal), devuelve verdadero
+// De lo contrario, devuelve falso
+
+// function myFunction(a) {
+// return a - Math.floor(a) === 0
+// }
+// console.log(myFunction(1049));
+
+// Escribe una función que tome dos números (a y b) como argumentos
+// Si a es menor que b, dividimos a por b
+// De lo contrario, multiplica ambos números
+// Devuelve el valor resultante
+
+// function myFunction(a, b) {
+//   return a < b ? a / b : a * b;
+// }
+
+// console.log(myFunction(10, 100));
+
+// Escribe una función que tome dos cadenas (a y b) como argumentos
+// Si a contiene b, agrega b al principio de a
+// Si no, añádelo al final
+// Devuelve la concatenación
+
+// function myFunction(a, b) {
+//   return a.includes(b) ? b + a : a + b;
+// }
+// console.log(myFunction(" think, therefore I am", "I"));
+
+// Escribir una función que tome un número (a) como argumento
+// Dividir a en sus dígitos individuales y devolverlos en una matriz
+// Consejo: es posible que desee cambiar el tipo de número para la división
+
+// function myFunction(a) {
+//     return Array.from(String(a), Number)
+    
+// }
+
+// console.log(myFunction(123456));
+
+
+
+// Escribir una función que tome una matriz (a) y un valor (n) como argumento
+// Devuelve el n-ésimo elemento de 'a'
+
+// function myFunction(a, n) {
+//     return a [n -1]
+// }
+// console.log(myFunction([10,9,8,7,6],5))
